9278d38168a109f841fe438d73bbd70f1e0b6cde
Arya-Kishan <arya12345kishan@gmail.com>
2024-06-13 09:54:52 +0530

    first commit

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import searchIcon from '../assets/searchIcon.svg'
+const Navbar = () => {
+    return (
+        <div className='w-full flex justify-between items-center py-3 px-6 fixed top-0 left-0 z-50'>
+
+            <h1 className='text-4xl font-bold uppercase'>animebook</h1>
+
+            <div className='w-[250px] flex justify-end gap-2'>
+                <input className='w-full rounded-md border-none outline-none p-2 hidden md:flex' placeholder='Search Amime...' type="text" name="" id="" />
+                <img className='w-[30px]' src={searchIcon} alt="" srcset="" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
